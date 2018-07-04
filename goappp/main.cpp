a8d40c91aff1a765519a57808debafb7e294174a
degski <degski@gmail.com>
2018-07-04 14:53:28 +0300

    added license

--- goappp/main.cpp
+++ goappp/main.cpp
@@ -27,76 +27,7 @@
 #include <vector>
 
 
-// std::ostream & nl ( std::ostream  & out_ ) { return out_ << '\n'; }
-
-template <typename T>
-struct relocator {
-
-    using value_type = T;
-    using pointer = value_type * ;
-
-    template <class U> struct rebind { typedef relocator<U> other; };
-
-    relocator ( ) { };
-    relocator ( const relocator & ) = delete;
-    relocator ( relocator && ) = delete;
-    ~relocator ( ) noexcept {
-        if ( m_pointer ) {
-            std::cout << "freed " << m_pointer << nl;
-            std::free ( m_pointer );
-        }
-    }
-
-    relocator & operator = ( const relocator & ) = delete;
-    relocator & operator = ( relocator && ) = delete;
-
-    template <class U> relocator ( relocator<U> const & ) noexcept { }
-
-    pointer allocate ( std::size_t n ) {
-        if ( m_pointer ) {
-            pointer p = m_pointer;
-            m_pointer = static_cast<pointer> ( std::realloc ( m_pointer, n * sizeof ( T ) ) );
-            std::cout << "realloced " << p << " to " << m_pointer << nl;
-        }
-        else {
-            m_pointer = static_cast<pointer> ( std::malloc ( n * sizeof ( T ) ) );
-            std::cout << "malloced " << m_pointer << nl;
-        }
-        return m_pointer;
-    }
-
-    void deallocate ( pointer, std::size_t ) noexcept { }
-
-    private:
-
-    pointer m_pointer = nullptr;
-};
-
-template <typename T, typename U>
-bool operator == ( relocator<T> const & l, relocator<U> const & r ) noexcept {
-    return l.m_pointer == r.m_pointer;
-}
-
-template <typename T, typename U>
-bool operator != ( relocator<T> const& l, relocator<U> const& r ) noexcept {
-    return l.m_pointer != r.m_pointer;
-}
-
-int __cdecl main ( ) {
-    {
-        std::vector<int, relocator<int>> v1, v2;
-
-        for ( int i = 0; i < 20000; ++i ) {
-            v1.push_back ( i );
-            v2.push_back ( i );
-        }
-    }
-
-    return 0;
-};
-
-
-int __cdecl main233546576 ( ) {
+int main ( ) {
 
     {
 
